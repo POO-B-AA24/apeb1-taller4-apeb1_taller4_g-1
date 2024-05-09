9117cfd5abd140c6a19545e2f5346f017556c467
Luis Aguilar <149074526+LuisAg1256@users.noreply.github.com>
2024-05-08 21:28:52 -0500

    codigo

--- Solucion_Codigo/Taller_4/src/Problema2_Empleado.java
+++ Solucion_Codigo/Taller_4/src/Problema2_Empleado.java
@@ -1,26 +1,26 @@
-public class Problema2_Empleado {
-    private String nombre;
-    private double salario;
-    private int edad;
-    
-    public Problema2_Empleado(String nombre, double salario, int edad) {
-        this.nombre = nombre;
-        this.salario = salario;
-        this.edad = edad;
-    }
 
+package javaapplication104;
+class Problema2_Empleado {
+        public String nombre;
+        public double salario;
+        public int edad;
+        public double promedio;
+        public double aumentoSalarioEmpleado;
+        public Problema2_Empleado(String nombre, double salario, int edad) {
+            this.nombre = nombre;
+            this.salario = salario;
+            this.edad = edad;
+        }
+    public void aumentoSalario(Problema2_Empleado empleado1[], int n){
+        this.promedio = salario/n;
+    }
+    public void aumentoSalarioEmpleado(Problema2_Empleado empleado1[], double aumSalario){
+        this.aumentoSalarioEmpleado= (this.salario<this.promedio)? this.salario * aumSalario: this.salario;
+    }
+    @Override
     public String toString() {
-        return "Empleado{" +
-                "nombre='" + nombre + '\'' +
-                ", salario=" + salario +
-                ", edad=" + edad +
-                '}';
+        return "Empleado{" + "nombre=" + nombre + ", salario=" + salario + ", edad=" + edad + ", promedio=" + promedio + ", aumentoSalarioEmpleado=" + aumentoSalarioEmpleado + '}';
     }
-
-    public double getSalario(){
-        return this.salario;
-    }
-    public void setSalaro(double sal){
-        this.salario=sal;
-    }
+    
+    
 }
--- Solucion_Codigo/Taller_4/src/Problema2_EmpleadoEjecutor.java
+++ Solucion_Codigo/Taller_4/src/Problema2_EmpleadoEjecutor.java
@@ -1,46 +1,31 @@
-import java.util.ArrayList;
+
+package javaapplication104;
 import java.util.Scanner;
 
 public class Problema2_EmpleadoEjecutor {
     public static void main(String[] args) {
-        double porcentajeAumento = 0.1;
-        boolean op = false;
-        Scanner sc = new Scanner(System.in);
-        double promedio = 0;
-        ArrayList<Problema2_Empleado> empleados = new ArrayList<>();
-        do {
-            System.out.println("Ingrese el nombre, salario y edad del empleado");
-            Problema2_Empleado empleado = new Problema2_Empleado(sc.next(), sc.nextDouble(), sc.nextInt());
-            empleados.add(empleado);
-            System.out.println("Desea ingresar mas empleados? 1.SI  |   2. NO");
-            op = sc.nextInt() == 1;
-        } while (op);
-        promedio = calcularPromedio(empleados, promedio);
-        calcularAumentoSalarial(promedio, empleados, porcentajeAumento);
-
-        for (Problema2_Empleado empleado : empleados) {
-            System.out.println(empleado);
+        Scanner scanner = new Scanner(System.in);
+        Problema2_Empleado empleado1[] = new Problema2_Empleado[100];        
+        boolean opc = true;
+        String resp = null;
+        double aumSalario;
+        int numEmpleados = 0;
+        System.out.println("Ingrese un aumento salarial");
+        aumSalario = scanner.nextDouble(); 
+        while (opc) {
+            System.out.print("Ingrese el nombre, el salario y la edad: ");
+            empleado1[numEmpleados] = new Problema2_Empleado(scanner.next(), scanner.nextDouble(), scanner.nextInt());
+            numEmpleados += 1;
+            empleado1[numEmpleados-1].aumentoSalario(empleado1, numEmpleados);
+            empleado1[numEmpleados-1].aumentoSalarioEmpleado(empleado1, aumSalario);
+            System.out.print("Desea agregar mas empleados? (S/N): ");
+            resp = scanner.next();
+            if (resp.equals("N")) break;
         }
-
-        sc.close();
-    }
-
-    public static double calcularPromedio(ArrayList<Problema2_Empleado> empleados, double promedio) {
-        promedio = 0;
-        for (Problema2_Empleado empleado : empleados) {
-            promedio += empleado.getSalario();
+        
+        System.out.println("Información de los empleados:");
+        for (int i = 0; i < numEmpleados; i++) {
+            System.out.println(empleado1[i]);
         }
-        promedio /= empleados.size();
-        return promedio;
-    }
-
-    public static void calcularAumentoSalarial(double promedio, ArrayList<Problema2_Empleado> empleados,
-            double porcentajeAumento) {
-        for (Problema2_Empleado empleado : empleados) {
-            double aux = 0;
-            empleado.setSalaro(
-                    aux += (empleado.getSalario() <= promedio) ? empleado.getSalario() * porcentajeAumento : 0);
-        }
-
     }
 }
